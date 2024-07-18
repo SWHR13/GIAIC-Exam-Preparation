8ba9115b310fb22cd37c0576a2e4987523d06332
SWHR13 <160714203+SWHR13@users.noreply.github.com>
2024-07-19 02:26:07 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,118 @@
+// let name="Batman";
+// console.log(name);
+
+//Interferring Type: It indicates the datatype of a variable 
+
+
+//JSON stands for Javascript Object Notation.
+//JSON used to store and transfer data.
+// JSON syntax
+/* {
+    "name": "Vipin",
+    "age": 21,
+    "gender": "male",
+  } */
+ 
+
+
+// But wait, Is JSON is similar to javaScript objects?
+// The Answer is No.
+    // JavaScript objects can contain functions but JSON not.
+    // JavaScript objects can only be used in JavaScript but JSON can be created and used by other programming languages.
+
+
+/* JSON Data
+    JSON data consists of key/value pairs similar to JavaScript object properties.
+    The key and values are written in double quotes separated by a :.
+    Example :
+    // JSON data
+    
+        "name": "Vipin"
+JSON data requires double quotes for the key. */
+
+
+
+/*JSON Object
+The JSON object is written inside curly braces { }.
+
+JSON objects can contain multiple key/value pairs.
+
+Example :
+
+ // JSON object
+
+ { "name": "Vipin", "age": 21 } */
+
+
+ 
+ 
+ /* JSON Array
+JSON array is written inside square brackets [ ].
+
+Example :
+
+ // JSON array
+
+ [ "Vipin", "Ankit", "Raj"] */
+
+
+ /* Accessing JSON Data
+We can access JSON data using the dot notation.
+
+Example :
+
+ // JSON object
+
+ const detail = { "name": "Vipin", "age": 21 }
+
+ // accessing JSON object
+
+ console.log(detail. name); // Vipin
+We can also use square bracket syntax [] to access JSON data. 
+
+Example : */
+
+ // JSON object
+
+ /* const detail = {
+
+     "name": "Vipin",
+
+     "age": 21
+ }
+//accessing JSON object
+
+console.log(detail["age"]); // 21 */
+
+
+
+// Use of JSON
+// JSON is the most commonly used format for transmitting data (data interchange) from a server to a client and vice-versa.
+// JSON data are very easy to parse and use.
+// JSON is language independent(We can create and use JSON in other programming languages too).
+
+
+
+// lett message = "Hello World";//syntax error
+// console.log(message);
+        
+//A syntax error is a mistake in the code's structure, such as misspelled keywords, incorrect punctuation, or unclosed strings, which prevents the code from running correctly.
+
+
+// result = "Hello" + 5  (//Solution: result = "Hello" + str(5) OR result = 5 + 5)
+// This will cause a type error because you can't add a string and a number.
+
+//A type error happens when you use the wrong type of data together, like adding a number to a word.
+
+
+// let num: number = 42;
+// num = "Vengeance"; 
+// console.log(num);
+
+// In this example, assigning a string to a variable that expects a number causes an assignability error.
+
+//An assignability error occurs when you try to assign a value to a variable that is not compatible with the variable's type. This is common in strongly typed languages.
+
+//Note: To Run any code just remove Comments (//)
+
+//By: SYED WALI HUSSAIN RIZVI
